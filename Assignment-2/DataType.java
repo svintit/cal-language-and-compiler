359db3f21f89ca91270e1f16d9a832c18da20339
Traian Svinti <44262998+svintit@users.noreply.github.com>
2019-01-22 14:25:20 +0000

    Add files via upload

---
+++ Assignment-2/DataType.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public enum DataType {
+    Program,
+    VariableDeclaration,
+    ConstantDeclaration,
+    ConstantAssign,
+    Function,
+    FunctionReturn,
+    Type,
+    ParameterList,
+    Main,
+    Statement,
+    ArgumentList,
+    Identifier,
+    Boolean,
+    Number,
+    TypeUnknown,
+    TypeVoid
+}
